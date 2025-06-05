de7f1b251e05a6cccf9eb1e88b0bf5538f7bc623
Ayansh Singh <ayanshashish@gmail.com>
2025-06-05 08:53:00 -0700

    fixed clashing pages

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ class NavBar extends HTMLElement {
         // Set the inner HTML of this element to contain a navigation bar
         this.innerHTML = `
         <ul>
-            <li><a href="/index.html">AppDev Club</a></li>
+            <li><a href="/Index.html">AppDev Club</a></li>
             <li><a href="/about.html">About Us</a></li>
             <li><a href="/ouroloop.html">Ouroloop</a></li>
             <li><a href="/polyverse.html">Polyverse</a></li>
